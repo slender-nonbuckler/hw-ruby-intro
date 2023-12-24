1c61027a55127e5b338c9613d6ff2bcb51a45c91
QJC <qjichun@gmail.com>
2023-12-23 16:44:10 -0800

    finished hw

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,79 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  res = 0
+  arr.each {|x|
+  res += x
+}
+  return res
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  elsif arr.size = 1
+    return arr[0]
+  else
+    return arr.max(2).sum
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.size <= 1
+    return false
+  else
+    0.upto(arr.size - 2) do |i|
+      (i+1).upto(arr.size - 1) do |j|
+        if arr[i] + arr[j] == n
+          return true
+        end
+      end
+    end
+    return false
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  res = "Hello, " + name
+  return res
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.size < 1 || (/\W/ =~ s) == 0  # first letter is non letter character
+    return false
+  end
+  if (/[^aeiouAEIOU]/ =~ s) == 0
+    return true
+  else 
+    return false
+  end 
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.size < 4
+    return false
+  else
+    return /1000/.match(s[-4, 4])
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  @isbn = ""
+  @price = 0.0
+  def initialize(isbn, price)
+    if isbn.empty? || price <= 0
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
+  end
+  attr_accessor :ISBM #getter and setter,allow read and write this attribute
+  attr_accessor :price
+  def price_as_string()
+    return "$%.2f" % @price
+  end
 end
